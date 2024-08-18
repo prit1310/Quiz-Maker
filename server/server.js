f886f13fa107c381149cc94a684b83896cdef3bd
prit1310 <pritsenjaliya1162@gmail.com>
2024-08-18 12:40:28 +0530

    update code according to deploy module

--- server/server.js
+++ server/server.js
@@ -1,28 +1,35 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const dotenv = require('dotenv');
 const authRoute = require('./router/auth-router');
 const queRoute = require('./router/question-router');
 const connectDb = require('./utils/db');
 const errorMiddleware = require('./middlewares/error-middleware');
+const path = require('path');
+
+const app = express();
 
 dotenv.config();
 app.use(express.json());
 
 const corsOptions = {
-  origin: "http://localhost:5173",
-  methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
+  origin: 'http://localhost:5173',
+  methods: 'GET, POST, PUT, DELETE, PATCH, HEAD',
   credentials: true,
 };
 
 app.use(cors(corsOptions));
 
-
 app.use('/api/auth', authRoute);
 app.use('/api', queRoute);
 
 const PORT = process.env.PORT || 3000;
+
+// Resolve dirname using CommonJS
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+//render client
+app.get('*', (req,res) => res.sendFile(path.join(__dirname, '/client/dist/index.html')))
 
 connectDb()
   .then(() => {
